304ebe05e65533be3345d2360c136e7d3d5c5d67
kyj00060006-hub <kyj00060006@gmail.com>
2026-04-22 14:15:47 +0800

    Practical10 is uploaded!

--- Practical10/dalys.py
+++ Practical10/dalys.py
@@ -1,5 +1,9 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
+os.chdir('/Users/kyj/Desktop/ibi/Portfolio/Practical10')
 dalys_data = pd.read_csv("dalys-rate-from-all-causes.csv")
 
 # 1. Showing the third and fourth columns for the first 10 rows
@@ -10,18 +14,26 @@ afg_first_10 = dalys_data.head(10)
 daly_col_name = dalys_data.columns[3]
 max_year_afg = afg_first_10.loc[afg_first_10[daly_col_name].idxmax(), "Year"]
 print(f"\nYear with maximum DALYs in the first 10 years for Afghanistan: {max_year_afg}")
+# The year with maximum DALYs in the first 10 rows for Afghanistan is: 1998
 
-# 2. Using a Boolean to show all years for Zimbabwe
+# 2. Boolean to show all years for Zimbabwe
 is_zimbabwe = dalys_data["Entity"] == "Zimbabwe"
 zimbabwe_years = dalys_data.loc[is_zimbabwe, "Year"]
 print("\nYears recorded for Zimbabwe:\n", zimbabwe_years.tolist())
+first_zim = zimbabwe_years.min()
+last_zim = zimbabwe_years.max()
+print(f"\nFirst year recorded for Zimbabwe: {first_zim}")
+print(f"Last year recorded for Zimbabwe: {last_zim}")
+# First year recorded for Zimbabwe: 1990; Last year recorded for Zimbabwe: 2019
 
-# 3. Computing countries with max and min DALYs in 2019
+# 3. Countries with max and min DALYs in 2019
 recent_data = dalys_data[dalys_data["Year"] == 2019]
 max_country_2019 = recent_data.loc[recent_data[daly_col_name].idxmax(), "Entity"]
 min_country_2019 = recent_data.loc[recent_data[daly_col_name].idxmin(), "Entity"]
 print(f"\n2019 - Country with Max DALYs: {max_country_2019}")
 print(f"2019 - Country with Min DALYs: {min_country_2019}")
+# Country with maximum DALYs in 2019: Lesotho
+# Country with minimum DALYs in 2019: Singapore
 
 # 4. Plotting DALYs over time for the country with Maximum DALYs
 target_data = dalys_data[dalys_data["Entity"] == max_country_2019]
