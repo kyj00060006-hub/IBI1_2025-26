ccaea0802d9fa2002ab75076eee6839cf9bd772e
kyj00060006-hub <kyj00060006@gmail.com>
2026-05-10 18:09:20 +0800

    Add my response to the feedback and fix a spelling mistake in pseudocode in mystery_code.py

--- Practical4/mystery_code.py
+++ Practical4/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer: run the While loop for 11 times, every time it generate an random integer bwtween 1 and 10, accumulates the sum, and prints the total.
+# Answer: run the While loop for 11 times, every time it generate an random integer between 1 and 10, accumulates the sum, and prints the total.
 # and i think 'ceil' is useless.
 
 # Import libraries
